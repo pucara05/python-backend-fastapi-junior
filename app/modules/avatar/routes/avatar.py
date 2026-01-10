5de6851d052eb0e0831d230779df1ab3a8037531
pucara05 <pucara05@gmail.com>
2026-01-10 09:01:23 -0400

    feat: define avatar CRUD routes

--- app/modules/avatar/routes/avatar.py
+++ app/modules/avatar/routes/avatar.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter (
     prefix="/avatars",
@@ -7,4 +7,21 @@ router = APIRouter (
 
 @router.get("/")
 def get_avatars():
-    return {"message": "avatar list endpoint"}
+    return {"message": "avatar list endpoint  (not implemented yet)"}
+
+@router.get("/{avatar_id}")
+def get_avatar(avatar_id: int):
+    return {"message": f"get avatar {avatar_id} (not implemented yet)"}
+
+
+@router.post("/")
+def create_avatar():
+    return {"message": "Create avatar (not implemented yet)"}
+
+@router.put("/{avatar_id}")
+def update_avatar(avatar_id: int):
+    return {"message": f"update avatar {avatar_id} "}
+
+@router.delete("/{avatar_id}")
+def delete_avatar(avatar_id: int):
+     return {"message": f"Delete avatar {avatar_id} (not implemented yet)"}
