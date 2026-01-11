6f8fc18dca646011a43a5598061ad17513ad037c
pucara05 <pucara05@gmail.com>
2026-01-11 17:06:33 -0400

    feat: add avatar service with SQLAlchemy CRUD logic

--- app/modules/avatar/services/avatar_service.py
+++ app/modules/avatar/services/avatar_service.py
@@ -1,47 +1,53 @@
-from typing import List, Dict
+from sqlalchemy.orm import Session
+from app.modules.avatar.models.avatar import Avatar
 from app.modules.avatar.schemas.avatar import AvatarCreate, AvatarUpdate
 
 
-# Simulación de almacenamiento (temporal)
-avatars_db: List[dict] = []
 
 
-def get_all_avatars():
-    return avatars_db
+
+def get_all_avatars(db: Session):
+    return db.query(Avatar).all()
 
 
-def get_avatar_by_id(avatar_id: int):
-    for avatar in avatars_db:
-        if avatar["id"] == avatar_id:
-            return avatar
-
-            return None
-
-def create_avatar(data:AvatarCreate):
-    new_avatar = {
-        "id": len(avatars_db) + 1,
-        "name": data.name,
-        "role": data.role,
-        "tone": data.tone,
-    }
-    avatars_db.append(new_avatar)
-    return new_avatar
+def get_avatar_by_id(db: Session, avatar_id: int):
+    return db.query(Avatar).filter(Avatar.id == avatar_id).first()
 
 
-def update_avatar(avatar_id: int, data: AvatarUpdate):
-    for avatar in avatars_db:
-        if avatar["id"] == avatar_id:
-            if data.name is not None:
-                avatar["name"] = data.name
-            if data.role is not None:
-                avatar["role"] = data.role
-            if data.tone is not None:
-                avatar["tone"] = data.tone
-            return avatar
-    return None
+def create_avatar(db: Session, avatar_data:AvatarCreate):
+   avatar = Avatar(
+    name=avatar_data.name,
+    role=avatar_data.role,
+    tone=avatar_data.tone
+   )
+   db.add(avatar)
+   db.commit()
+   db.refresh(avatar)
+   return avatar
 
-def delete_avatar(avatar_id: int):
-    for index, avatar in enumerate(avatars_db):
-        if avatar["id"] == avatar_id:
-            return avatars_db.pop(index)
-    return None
+
+def update_avatar(db: Session, avatar_id: int, avatar_data: AvatarUpdate):
+    avatar = get_avatar_by_id(db, avatar_id)
+    if not avatar:
+        return None
+
+    if avatar_data.name is not None:
+        avatar.name = avatar_data.name
+    if avatar_data.role is not None:
+        avatar.role = avatar_data.role
+    if avatar_data.tone is not None:
+        avatar.tone = avatar_data.tone
+
+    db.commit()
+    db.refresh(avatar)
+    return avatar
+
+
+def delete_avatar(db: Session,avatar_id: int):
+    avatar = get_avatar_by_id(db, avatar_id)
+    if not avatar:
+        return None
+
+    db.delete(avatar)
+    db.commit()
+    return True
