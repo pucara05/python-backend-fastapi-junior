43086b468e03b5195938c79d15b83fc026ba157a
pucara05 <pucara05@gmail.com>
2026-01-11 18:38:56 -0400

    feat: complete avatar CRUD with FastAPI and SQLAlchemy

--- app/modules/avatar/routes/avatar.py
+++ app/modules/avatar/routes/avatar.py
@@ -1,6 +1,9 @@
 from app.modules.avatar.schemas.avatar import AvatarCreate, AvatarUpdate
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+from app.db.session import get_db
 from app.modules.avatar.services import avatar_service
+
 
 router = APIRouter (
     prefix="/avatars",
@@ -8,33 +11,46 @@ router = APIRouter (
 )
 
 @router.get("/")
-def get_avatars():
-    return avatar_service.get_all_avatars()
+def get_avatars(db: Session = Depends(get_db)):
+
+    return avatar_service.get_all_avatars(db)
+
 
 @router.get("/{avatar_id}")
-def get_avatar(avatar_id: int):
-    avatar = avatar_service.get_avatar_by_id(avatar_id)
+def get_avatar(avatar_id: int, db: Session = Depends(get_db)):
+
+    avatar = avatar_service.get_avatar_by_id(db, avatar_id)
     if not avatar:
-        raise HTTPException(status_code=404, detail="Avatar not found")
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Avatar not found")
+
     return avatar
 
 
-@router.post("/")
-def create_avatar(avatar: AvatarCreate):
-    return avatar_service.create_avatar(avatar)
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_avatar(avatar: AvatarCreate, db: Session = Depends(get_db)):
+    return avatar_service.create_avatar(db, avatar)
 
 
 @router.put("/{avatar_id}")
-def update_avatar(avatar_id: int, avatar: AvatarUpdate):
-    updated = avatar_service.update_avatar(avatar_id, avatar)
+def update_avatar(avatar_id: int, avatar: AvatarUpdate, db: Session = Depends(get_db)):
+
+    updated = avatar_service.update_avatar(db, avatar_id, avatar)
+
     if not updated:
-        raise HTTPException(status_code=404, detail="Avatar not found")
+
+        raise HTTPException( status_code=status.HTTP_404_NOT_FOUND, detail="Avatar not found")
+
     return updated
 
 
-@router.delete("/{avatar_id}")
-def delete_avatar(avatar_id: int):
-    deleted = avatar_service.delete_avatar(avatar_id)
+@router.delete("/{avatar_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_avatar(avatar_id: int, db: Session = Depends(get_db)):
+
+    deleted = avatar_service.delete_avatar(db, avatar_id)
+
     if not deleted:
-        raise HTTPException(status_code=404, detail="Avatar not found")
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Avatar not found")
+
         return deleted
